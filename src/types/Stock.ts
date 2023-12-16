732eac128191b883970c3ea0a5f217caa07f3f53
joakimeineving <Joakim.eineving@gmail.com>
2023-12-16 14:02:19 +0100

    Add StockHistory interface and AssetPurchase interface

--- src/types/Stock.ts
+++ src/types/Stock.ts
@@ -24,10 +24,23 @@ export interface Stock {
 }
 export interface StockHistory {
   date: string;
+  open: number;
+  high: number;
+  low: number;
   close: number;
+  adjClose: number;
+  volume: number;
+  unadjustedVolume: number;
+  change: number;
+  changePercent: number;
+  vwap: number;
+  label: string;
+  changeOverTime: number;
 }
-
-
-
-
-
+export interface AssetPurchase {
+  symbol: string;
+  purchasePrice: number;
+  quantity: number;
+  datePurchased: string;
+  purchaseId: string;
+}
