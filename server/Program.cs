6d8bcde7a48d4a623151fbfaa22d4c0986c326fd
joakimeineving <Joakim.eineving@gmail.com>
2023-12-23 20:24:48 +0100

    Cors

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,8 @@
+global using StocksApi.Models;
+using StocksApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+var allowedOrigin = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
 
 // Add services to the container.
 
@@ -6,6 +10,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IStockService, StockService>();
+builder.Services.AddScoped<IMarketService, MarketService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("myAppCors", policy =>
+    {
+        policy.WithOrigins(allowedOrigin)
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -21,5 +37,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("myAppCors");
 
 app.Run();
